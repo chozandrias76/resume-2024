0ce22e52a1dcb48ab713d1f5dfd0bbfb5fa576de
Colin Swenson-Healey <chozandrias76@github.com>
2024-05-16 12:32:56 -0700

    feat: setup and verified datadog locally

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,31 @@
 "use client";
+import { RumInitConfiguration, datadogRum } from "@datadog/browser-rum";
 import Body from "./body";
 import { QueryClient, QueryClientProvider } from "react-query";
+
+const datadogOptions = {
+  applicationId: process.env.NEXT_PUBLIC_DATADOG_APPLICATION_ID,
+  clientToken: process.env.NEXT_PUBLIC_DATADOG_CLIENT_TOKEN,
+  site: 'us3.datadoghq.com',
+  service: 'resume',
+  env: process.env.NEXT_PUBLIC_APPLICATION_ENV || "development",
+  version: process.env.npm_package_version,
+  sessionSampleRate: 100,
+  sessionReplaySampleRate: 20,
+  trackUserInteractions: true,
+  trackResources: true,
+  trackLongTasks: true,
+  defaultPrivacyLevel: 'mask-user-input',
+} as RumInitConfiguration
+
+try {
+  datadogRum.init(datadogOptions);
+} catch(err) {
+  console.error("Could not start Datadog", err)
+}
+finally {
+  console.info("Datadog initialized")
+}
 
 export default function Home() {
   const queryClient = new QueryClient();
