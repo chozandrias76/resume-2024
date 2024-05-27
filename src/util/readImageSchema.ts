50b7edcc7030c9a06e081933f8948ca946edc1b3
Colin Swenson-Healey <chozandrias76@github.com>
2024-05-27 13:32:55 -0700

    fix: attempt using different paths for file retrieval

--- src/util/readImageSchema.ts
+++ src/util/readImageSchema.ts
@@ -1,7 +1,18 @@
 import * as fs from "node:fs";
+import { join } from "path";
 
-export function readImageSchema(schemaRelativePath: string, relativePath: string) {
-  const fullFilePath = `./public/elden-ring/raw/${relativePath}`;
-  const schemaFullFilePath = `./public/elden-ring/raw/${schemaRelativePath}`;
-  return {file: fs.readFileSync(fullFilePath), schema: fs.readFileSync(schemaFullFilePath)};
+export function readImageSchema(
+  schemaRelativePath: string,
+  relativePath: string
+) {
+  const jsonPath = join(process.cwd(), "public/elden-ring/raw", relativePath);
+  const schemaPath = join(
+    process.cwd(),
+    "public/elden-ring/raw",
+    schemaRelativePath
+  );
+  return {
+    file: fs.readFileSync(jsonPath),
+    schema: fs.readFileSync(schemaPath),
+  };
 }
