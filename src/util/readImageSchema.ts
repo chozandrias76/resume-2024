3b9392d95908a4e8140b6903f019478ae13225a8
Colin Swenson-Healey <chozandrias76@github.com>
2024-05-27 14:11:21 -0700

    feat: remove commented code

--- src/util/readImageSchema.ts
+++ src/util/readImageSchema.ts
@@ -1,6 +1,8 @@
 import * as fs from "node:fs";
 import { join } from "path";
 
+const imageSchemaRoute = "public/elden-ring/raw";
+
 function listFiles(dir: string): string[] {
   const results = [];
   const list = fs.readdirSync(dir, { withFileTypes: true });
@@ -20,39 +22,28 @@ export function readImageSchema(
   schemaRelativePath: string,
   relativePath: string
 ) {
-  const jsonPath = join(process.cwd(), "public/elden-ring/raw", relativePath);
-  const schemaPath = join(process.cwd(), "public/elden-ring/raw", schemaRelativePath);
+  const jsonPath = join(process.cwd(), imageSchemaRoute, relativePath);
+  const schemaPath = join(process.cwd(), imageSchemaRoute, schemaRelativePath);
   try {
     return {
       file: fs.readFileSync(jsonPath),
       schema: fs.readFileSync(schemaPath),
     };
   } catch (error: any) {
-    if (error.code === 'ENOENT') {
+    // This might expose too much to the client, but for now it's fine
+    if (error.code === "ENOENT") {
       try {
-        const files = listFiles(join(process.cwd(), 'public'));
-        throw new Error(`${error.message}. Available files: ${files.join(', ')}`);
+        const files = listFiles(join(process.cwd(), imageSchemaRoute));
+        throw new Error(
+          `${error.message}. Available files: ${files.join(", ")}`
+        );
       } catch (fsError: any) {
-        throw new Error(`${error.message}. Additionally, failed to list files: ${fsError.message}`);
+        throw new Error(
+          `${error.message}. Additionally, failed to list files: ${fsError.message}`
+        );
       }
     } else {
       throw error;
     }
   }
-}
-
-function _readImageSchema(
-  schemaRelativePath: string,
-  relativePath: string
-) {
-  const jsonPath = join(process.cwd(), "public/elden-ring/raw", relativePath);
-  const schemaPath = join(
-    process.cwd(),
-    "public/elden-ring/raw",
-    schemaRelativePath
-  );
-  return {
-    file: fs.readFileSync(jsonPath),
-    schema: fs.readFileSync(schemaPath),
-  };
 }
